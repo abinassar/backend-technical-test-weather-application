f72a7389dfed78115f247e7e3595b2e688f36bf6
abinassar <abinassar@gmail.com>
2025-06-17 17:55:12 -0400

    Enabled cors in app

--- src/main/java/com/cursos/springsecuritycourse/config/security/HttpSecurityConfig.java
+++ src/main/java/com/cursos/springsecuritycourse/config/security/HttpSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.cursos.springsecuritycourse.config.security;
 
 import com.cursos.springsecuritycourse.config.security.filter.JwtAuthenticationFilter;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -14,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Component
 @EnableWebSecurity
@@ -30,6 +36,8 @@ public class HttpSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+            .cors() // Habilita CORS
+            .and()
             .csrf( csrfConfig -> csrfConfig.disable() )
             .sessionManagement( sessionMangConfig -> sessionMangConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
             .authenticationProvider(authenticationProvider)
@@ -48,5 +56,24 @@ public class HttpSecurityConfig {
             authConfig.anyRequest().denyAll();
         };
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000", 
+            "http://localhost:4200", 
+            "https://enchanting-unity.up.railway.app",
+            "https://enchanting-unity.up.railway.app"
+        ));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
 }
